de604804f9d5936222ecbafd29015c09dcf1674d
NetrunerIhor <159276171+NetrunerIhor@users.noreply.github.com>
2024-04-24 08:45:07 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 function triangle(arg1, argType1, arg2, argType2) {
-  if ((argType1 != ('leg'||'hypotenuse'||'adjacent angle'||'opposite angle'||"angle")) || (argType2 != ('leg'||'hypotenuse'||'adjacent angle'||'opposite angle'||"angle"))){
+  // копектність типів аргументів
+  if (!['leg', 'hypotenuse', 'adjacent angle', 'opposite angle', 'angle'].includes(argType1) ||
+  !['leg', 'hypotenuse', 'adjacent angle', 'opposite angle', 'angle'].includes(argType2)){
     console.log("Invalid parameter type. Please take from the list: (leg, hypotenuse, adjacent angle, opposite angle ,angle)")
-    // return "failed"
+    return "failed"
   }
-  if((arg1 || arg2 ) <=0){
+  // чи не нульові значення 
+  if(arg1 <= 0 || arg2  <=0){
     console.log("Parameter values ​​cannot be zero or less than zero!!!")
     return "failed"
   }
+  //наявність комбінації 
   if((argType1 === 'hypotenuse' && argType2 === 'hypotenuse')||
     (argType1 == ('adjacent angle'||'opposite angle'||"angle")&& argType2 == ('adjacent angle'||
     'opposite angle'||"angle"))||(argType1 === 'leg' && argType2 === 'angle')||
@@ -14,19 +18,21 @@ function triangle(arg1, argType1, arg2, argType2) {
       console.log('There is no method for this combination. Select from the list (leg|leg, leg|hypotenuse, leg|adjacent angle, leg|opposite angle, angle|hypotenuse')
       return "failed"
     }
+  // розмір кутів 
   if((argType1 == ('adjacent angle'||'opposite angle'||"angle") && arg1 >=90)||
   (argType2 == ('adjacent angle'||'opposite angle'||"angle") && arg2 >=90)){
     console.log('The angle is not sharp!!!')
     return "failed"
   }
+  // катет менший за гіпотенузу
   if((argType1 == "hypotenuse"&& argType2 == "leg"&&arg2>=arg1)||(argType1 == "leg"&& argType2 == "hypotenuse"&&arg1>=arg2)){
     console.log('The leg must be smaller than the hypotenuse!!!')
     return "failed"
   }
   if (argType1 == "leg" && argType2 == "leg") {
     const h = Math.sqrt(arg1 * arg1 + arg2 * arg2);
-    const alpha = Math.atan(arg1 / arg2);
-    const beta = Math.atan(arg2 / arg1);
+    const alpha = Math.atan(arg1 / arg2)* (180 / Math.PI);
+    const beta = Math.atan(arg2 / arg1)* (180 / Math.PI);
     console.log("a = " + arg1);
     console.log("b = " + arg2);
     console.log("c = " + h);
@@ -54,9 +60,9 @@ function triangle(arg1, argType1, arg2, argType2) {
     console.log("beta = " + beta);
   }
   else if (argType1 == "adjacent angle" && argType2 == "leg") {
-    arg1 = arg1 * (Math.PI / 180);
-    const c = arg2 / Math.cos(arg1);
-    const a = arg2 * Math.tan(arg1);
+    // arg1 = arg1;
+    const c = arg2 / Math.cos(arg1*(Math.PI/180));
+    const a = arg2 * Math.tan(arg1*(Math.PI/180));
     const beta = 90 - arg1;
     console.log("a = " + a);
     console.log("c = " + c);
@@ -65,9 +71,9 @@ function triangle(arg1, argType1, arg2, argType2) {
     console.log("beta = " + beta);
   }
   else if (argType1 == "leg" && argType2 == "adjacent angle") {
-    arg2 = arg2 * (Math.PI / 180);
-    const c = arg1 / Math.cos(arg2);
-    const b = arg1 * Math.tan(arg2);
+    // arg2 = arg2;
+    const c = arg1 / Math.cos(arg2*(Math.PI/180));
+    const b = arg1 * Math.tan(arg2*(Math.PI/180));
     const alpha = 90 - arg2;
     console.log("a = " + arg1);
     console.log("b = " + b);
@@ -76,9 +82,9 @@ function triangle(arg1, argType1, arg2, argType2) {
     console.log("beta = " + arg2);
   }
   else if (argType1 == "opposite angle" && argType2 == "leg") {
-    arg1 = arg1 * (Math.PI / 180);
-    const c = arg2 / Math.sin(arg1);
-    const a = arg2 * Math.tan(arg1);
+    // arg1 = arg1 * (Math.PI / 180);
+    const c = arg2 / Math.sin(arg1*(Math.PI/180));
+    const a = arg2 * Math.tan(arg1*(Math.PI/180));
     const alpha = 90 - arg1;
     console.log("a = " + a);
     console.log("b = " + arg2);
@@ -87,9 +93,9 @@ function triangle(arg1, argType1, arg2, argType2) {
     console.log("beta = " + arg1);
   }
   else if (argType1 == "leg" && argType2 == "opposite angle") {
-    arg2 = arg2 * (Math.PI / 180);
-    const c = arg1 / Math.sin(arg2);
-    const b = arg1 * Math.tan(arg2);
+    // arg2 = arg2 * (Math.PI / 180);
+    const c = arg1 / Math.sin(arg2*(Math.PI/180));
+    const b = arg1 * Math.tan(arg2*(Math.PI/180));
     const beta = 90 - arg2;
     console.log("a = " + arg1);
     console.log("b = " + b);
@@ -98,10 +104,10 @@ function triangle(arg1, argType1, arg2, argType2) {
     console.log("beta = " + beta);
   }
   else if (argType1 == "angle" && argType2 == "hypotenuse") {
-    arg1 = arg1 * (Math.PI / 180);
+    // arg1 = arg1 * (Math.PI / 180);
     const beta = 90 - arg1;
-    const a = arg2 * Math.sin(arg1);
-    const b = arg2 * Math.cos(arg1);
+    const a = arg2 * Math.sin(arg1*(Math.PI/180));
+    const b = arg2 * Math.cos(arg1*(Math.PI/180));
     console.log("a = " + a);
     console.log("b = " + b);
     console.log("c = " + arg2);
@@ -109,10 +115,10 @@ function triangle(arg1, argType1, arg2, argType2) {
     console.log("beta = " + beta);
   }
   else if (argType1 == "hypotenuse" && argType2 == "angle") {
-    arg2 = arg2 * (Math.PI / 180);
+    // arg2 = arg2 * (Math.PI / 180);
     const alpha = 90 - arg2;
-    const a = arg1 * Math.cos(arg2);
-    const b = arg1 * Math.sin(arg2);
+    const a = arg1 * Math.cos(arg2*(Math.PI/180));
+    const b = arg1 * Math.sin(arg2*(Math.PI/180));
     console.log("a = " + a);
     console.log("b = " + b);
     console.log("c = " + arg1);
@@ -123,10 +129,11 @@ function triangle(arg1, argType1, arg2, argType2) {
   return "success"
 }
 
-triangle(5, "hypotenuse", 36.87, "angle");
-triangle(3 ,"leg", 4 ,"leg");
-triangle(5 ,"hypotenuse", 4 ,"leg");
-triangle(3 ,"leg", 5 ,"hypotenuse");
-triangle(36.87 ,"adjacent angle", 4 ,"leg")
-triangle(6 ,"leg", 5 ,"hypotenuse");
-triangle(5 ,"leg", 5 ,"hypotenuse");
+triangle(process.argv[2] ,process.argv[3]  , process.argv[4] , process.argv[5] );
+// triangle(5, "hypotenuse", 36.87, "angle");
+// triangle(3 ,"leg", 4 ,"leg");
+// triangle(5 ,"hypotenuse", 4 ,"leg");
+// triangle(3 ,"leg", 5 ,"hypotenuse");
+// triangle(36.87 ,"adjacent angle", 4 ,"leg")
+// triangle(6 ,"leg", 5 ,"hypotenuse");
+// triangle(5 ,"leg", 5 ,"hypotenuse");
